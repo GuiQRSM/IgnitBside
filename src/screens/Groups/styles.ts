06767fb279ab99ca2a8fcd9c09fc2765a996feb0
GuiQRSM <endbeob15@gmail.com>
2024-06-12 15:04:08 -0300

    Finish the main learn about ''styled-component.

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,7 +2,12 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
 flex: 1;
-background-color: #fff;
+background-color: #000000;
 align-items: center;
 justify-content: center;
 `;
+
+export const Title = styled.Text`
+color: #fff;
+font-size: 32px;
+`
