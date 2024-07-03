0d4b81d32ec876aec88015d90f03624106e7e766
GuiQRSM <endbeob15@gmail.com>
2024-07-03 16:55:40 -0300

    Some updates.

--- App.tsx
+++ App.tsx
@@ -4,13 +4,14 @@ import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ActivityIndicator } from 'react-native';
 
+
 export default function App() {
 
     const [fontsLoader] = useFonts({Roboto_400Regular, Roboto_700Bold});
 
     return (
         <ThemeProvider theme={theme}>
-            { fontsLoader ? <Groups /> : <ActivityIndicator/>};
+            {fontsLoader ? <Groups/> : <ActivityIndicator/>};
         </ThemeProvider>
     );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,8 @@
 import { Container } from "./styles";
+import { View, Text} from "react-native";
 
 export function Groups() {
     return (
-        <Container/>
+        <Container></Container>
     );
 }
