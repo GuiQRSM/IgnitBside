19601f001dd5f2fdbd2ad8570e95481bcc9b2e4f
GuiQRSM <endbeob15@gmail.com>
2024-07-09 22:11:31 -0300

    More a remotion.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { Groups } from '@screens/Groups';
 import {ThemeProvider} from 'styled-components/native';
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { ActivityIndicator } from 'react-native';
 import { Loading } from '@components/Loading';
 
 
