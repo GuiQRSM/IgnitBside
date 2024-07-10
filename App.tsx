3b1380c3b0aaf5275336954ad9150166277c1840
GuiQRSM <endbeob15@gmail.com>
2024-07-09 21:46:00 -0300

    Imports in the 'App.tsx' the component 'Loading'.

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {ThemeProvider} from 'styled-components/native';
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ActivityIndicator } from 'react-native';
+import { Loading } from '@components/Loading';
 
 
 export default function App() {
