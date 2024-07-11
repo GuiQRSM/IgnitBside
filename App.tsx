b3841c026e28c506e5c7c254aedcd995b6c67458
GuiQRSM <endbeob15@gmail.com>
2024-07-10 22:55:55 -0300

    Inports 'StatusBar' in the 'App.tsx'

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {ThemeProvider} from 'styled-components/native';
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 
 export default function App() {
